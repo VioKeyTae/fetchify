f0e05b5a3944ce132e059c7d93c9ec242dd900ca
Max Gierlachowski <Max.Gierlachowski@gmail.com>
2018-03-08 08:26:24 +0100

    Updated authorization code

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -90,6 +90,8 @@ app.get('/callback', function(req, res) {
         var access_token = body.access_token,
             refresh_token = body.refresh_token;
 
+        console.log(access_token);
+
         var options = {
           url: 'https://api.spotify.com/v1/me',
           headers: { 'Authorization': 'Bearer ' + access_token },
