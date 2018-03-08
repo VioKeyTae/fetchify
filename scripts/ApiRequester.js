a2ef51f5658c6646d7a446aaf3ffd97b1924a076
Max Gierlachowski <Max.Gierlachowski@gmail.com>
2018-03-08 08:17:02 +0100

    Removed node_modules from resporitory
    Removed node_modules, added Api-Access class

--- scripts/ApiRequester.js
+++ scripts/ApiRequester.js
@@ -1,1 +1,28 @@
-class 
+class ApiRequester {
+
+    constructor(apiUrl, apiToken) {
+        this.apiUrl = apiUrl;
+        this.apiToken = apiToken;
+    }
+
+    getArtist(artist) {
+        $.ajax({
+            type: "GET",
+            url: this.apiUrl + "/v1/search",
+            data: {
+                q: artist,
+                type: "artist"
+            },
+            beforeSend: (request) => {
+                request.setRequestHeader("Authorization", "Bearer " + this.apiToken);
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+
+}
