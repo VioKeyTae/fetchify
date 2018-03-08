2107a071d8c9a5294747e234704aab95b4aa0633
BlubSta <nicolas.haller@hotmail.com>
2018-03-08 08:19:20 +0100

    new output with more information

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -141,5 +141,8 @@ app.get('/refresh_token', function(req, res) {
   });
 });
 
-console.log('Listening on 8888');
-app.listen(8888);
+var server = app.listen(8888, function (){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
