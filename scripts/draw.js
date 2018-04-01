42c4a07881beb528137eda5b519246cda79f05f9
René <viokeytae@gmail.com>
2018-04-01 15:47:42 +0200

    bit of stuff

---
+++ scripts/draw.js
@@ -0,0 +1,87 @@
+var width = 500,
+    height = 500,
+    radius = Math.min(width, height) / 2,
+    innerRadius = 0.3 * radius;
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.width; });
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([0, 0])
+  .html(function(d) {
+    return d.data.label + ": <span style='color:orangered'>" + d.data.score + "</span>";
+  });
+
+var arc = d3.svg.arc()
+  .innerRadius(innerRadius)
+  .outerRadius(function (d) {
+    return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius;
+  });
+
+var outlineArc = d3.svg.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(radius);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+svg.call(tip);
+data= [
+  {"id":"ABC","order":1,"score":60,"weight":1,"color":"#6ef442","label": "Danciness"},
+  {"id":"CDE","order":2,"score":40,"weight": 1,"color":"#fc92fc", "label": "Happiness"},
+  {"id":"EFG","order":2,"score":90,"weight": 1,"color":"#9094f9", "label": "Loudiness"},
+  {"id":"EFG","order":2,"score":96,"weight": 1,"color":"#8ff2f7", "label": "Loch Ness"},
+  {"id":"EFG","order":2,"score":50,"weight": 1,"color":"#fc5f81", "label": "Accousticness"}
+];
+
+
+  data.forEach(function(d) {
+    d.id     =  d.id;
+    d.order  = +d.order;
+    d.color  =  d.color;
+    d.weight = +d.weight;
+    d.score  = +d.score;
+    d.width  = +d.weight;
+    d.label  =  d.label;
+  });
+  // for (var i = 0; i < data.score; i++) { console.log(data[i].id) }
+
+  var path = svg.selectAll(".solidArc")
+      .data(pie(data))
+    .enter().append("path")
+      .attr("fill", function(d) { return d.data.color; })
+      .attr("class", "solidArc")
+      .attr("stroke", "gray")
+      .attr("d", arc)
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+  var outerPath = svg.selectAll(".outlineArc")
+      .data(pie(data))
+    .enter().append("path")
+      .attr("fill", "none")
+      .attr("stroke", "gray")
+      .attr("class", "outlineArc")
+      .attr("d", outlineArc);
+
+
+  // calculate the weighted mean score
+  var score =
+    data.reduce(function(a, b) {
+      //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
+      return a + (b.score * b.weight);
+    }, 0) /
+    data.reduce(function(a, b) {
+      return a + b.weight;
+    }, 0);
+
+  svg.append("svg:text")
+    .attr("class", "aster-score")
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle") // text-align: right
+    .text(Math.round(score));
