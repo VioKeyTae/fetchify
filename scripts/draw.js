2eaf12fb3bfe5c3eac43bbe56e0f3ec43785d055
Max Gierlachowski <Max.Gierlachowski@gmail.com>
2018-04-03 12:21:00 +0200

    Added Gierla Design

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,55 +1,57 @@
+function draw() {
+    var width = 500,
+        height = 500,
+        radius = Math.min(width, height) / 2,
+        innerRadius = 0.3 * radius;
 
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function (d) {
+            return d.width;
+        });
 
-function draw(){
-  var width = 500,
-      height = 500,
-      radius = Math.min(width, height) / 2,
-      innerRadius = 0.3 * radius;
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([0, 0])
+        .html(function (d) {
+            return d.data.label + ": <span style='color:orangered'>" + d.data.score + "</span>";
+        });
 
-  var pie = d3.layout.pie()
-      .sort(null)
-      .value(function(d) { return d.width; });
+    var arc = d3.svg.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(function (d) {
+            return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius;
+        });
 
-  var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([0, 0])
-    .html(function(d) {
-      return d.data.label + ": <span style='color:orangered'>" + d.data.score + "</span>";
-    });
+    var outlineArc = d3.svg.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(radius);
 
-  var arc = d3.svg.arc()
-    .innerRadius(innerRadius)
-    .outerRadius(function (d) {
-      return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius;
-    });
+    var svg = d3.select("#datawrapper").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-  var outlineArc = d3.svg.arc()
-          .innerRadius(innerRadius)
-          .outerRadius(radius);
+    svg.call(tip);
 
-  var svg = d3.select("#datawrapper").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-  svg.call(tip);
-
-    datalist.forEach(function(d) {
-      d.id     =  d.id;
-      d.order  = +d.order;
-      d.color  =  d.color;
-      d.weight = +d.weight;
-      d.score  = +d.score;
-      d.width  = +d.weight;
-      d.label  =  d.label;
+    datalist.forEach(function (d) {
+        d.id = d.id;
+        d.order = +d.order;
+        d.color = d.color;
+        d.weight = +d.weight;
+        d.score = +d.score;
+        d.width = +d.weight;
+        d.label = d.label;
     });
     // for (var i = 0; i < datalist.score; i++) { console.log(datalist[i].id) }
 
     var path = svg.selectAll(".solidArc")
         .data(pie(datalist))
         .enter().append("path")
-        .attr("fill", function(d) { return d.data.color; })
+        .attr("fill", function (d) {
+            return d.data.color;
+        })
         .attr("class", "solidArc")
         .attr("stroke", "gray")
         .attr("d", arc)
@@ -58,7 +60,7 @@ function draw(){
 
     var outerPath = svg.selectAll(".outlineArc")
         .data(pie(datalist))
-      .enter().append("path")
+        .enter().append("path")
         .attr("fill", "none")
         .attr("stroke", "gray")
         .attr("class", "outlineArc")
@@ -67,17 +69,17 @@ function draw(){
 
     // calculate the weighted mean score
     var score =
-      datalist.reduce(function(a, b) {
-        //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
-        return a + (b.score * b.weight);
-      }, 0) /
-      datalist.reduce(function(a, b) {
-        return a + b.weight;
-      }, 0);
+        datalist.reduce(function (a, b) {
+            //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
+            return a + (b.score * b.weight);
+        }, 0) /
+        datalist.reduce(function (a, b) {
+            return a + b.weight;
+        }, 0);
 
     svg.append("svg:text")
-      .attr("class", "aster-score")
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle") // text-align: right
-      .text(Math.round(score));
+        .attr("class", "aster-score")
+        .attr("dy", ".35em")
+        .attr("text-anchor", "middle") // text-align: right
+        .text(Math.round(score));
 }
